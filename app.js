5a782a840e4ff2530d02babfad5ca559ba98faf6
Dragomir Urdov <dragomir.urdov@gmail.com>
2021-04-21 22:41:46 +0200

    #1 Setup basic npm modules. Further configuration is required.

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+
+// const multer = require('multer');
+// const upload = multer({
+//   dest: 'uploads/',
+// });
 
 const app = express();
 
-app.listen(3000, () => {});
+dotenv.config();
+
+app.use(express.json());
+app.use(helmet());
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'success',
+    data: {
+      test: 'Test 😉',
+    },
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {});
